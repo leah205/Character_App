627c33cd6d55cf596f8873892163a1a823f693b3
leah205 <leahtiktin@gmail.com>
2025-07-26 09:43:01 -0600

    configure home route

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ require("dotenv").config()
 const express = require('express')
 app = express()
 const path = require('node:path')
+
+const indexRouter = require("./routes/indexRoute.js")
+
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }));
 let staticPaths = path.join(__dirname, 'public')
 app.use(express.static(staticPaths))
+
+
+
+app.use("/", indexRouter)
 
 
 app.listen(process.env.PORT, () => {
